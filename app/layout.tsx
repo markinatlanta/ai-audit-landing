5b60bfe3a21afc85108b9eb7f26c4def31547f1e
markinatlanta <markjsandefur@gmail.com>
2025-07-04 15:09:56 -0400

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,4 @@
-
+import Script from 'next/script'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
@@ -25,6 +25,25 @@ export default function RootLayout({
    
 
     <html lang="en">
+      <head>
+  <Script
+    strategy="afterInteractive"
+    src="https://www.googletagmanager.com/gtag/js?id=AW-17307204764"
+  />
+  <Script
+    id="gtag-init"
+    strategy="afterInteractive"
+    dangerouslySetInnerHTML={{
+      __html: `
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        gtag('config', 'AW-17307204764');
+      `,
+    }}
+  />
+</head>
+
       <body className={inter.className}>{children}</body>
     </html>
   )
